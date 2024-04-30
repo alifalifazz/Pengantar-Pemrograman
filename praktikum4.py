944d9e0419b85cf52063a30ed3ee168e608955c0
Alifa <alifizatunnisa@gmail.com>
2024-04-30 11:19:20 +0700

    commit1

---
+++ praktikum4.py
@@ -0,0 +1,17 @@
+#BIODATA
+print("ini program python")
+nama = "Alifa Izzatunnisa"
+alamat = "Pamengpeuk"
+umur = "19"
+hobi = "rebahan"
+
+print("Nama saya " + nama, "yang beralamat di " + alamat,",","umur sekarang " + umur, "tahun dan memiliki hobi " + hobi)
+
+#menghitung luas & keliling persegi panjang 
+panjang = 15
+lebar = 5.7
+
+luas = panjang * lebar 
+keliling = 2 * (panjang + lebar) 
+print(f"sebuah persegi panjang memiliki panjang {panjang} cm, lebar {lebar} cm, luasnya {luas} cm, dan kelilingnya {keliling} cm")
+
